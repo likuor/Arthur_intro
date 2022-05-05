987a7aad3f152c665419e9f788efabbdd41bddbd
Koki Sakai <likuor0528@gmail.com>
2022-05-05 11:27:40 -0700

    test

---
+++ ciccc-js-fundamentals/solutions/test.js
@@ -0,0 +1,56 @@
+// const a = 2;
+// const b = 3;
+// let total = 0;
+
+// const sum = () => {
+//      total = a + b;
+//     //total = 5
+//     return total;
+// };
+
+// console.log(sum());
+// console.log(total > 0); // expect print true 5 > 0
+// // false total 0 = false
+// console.log(total);
+
+// let prize = "";
+// const winner = 3;
+
+// switch (winner){
+//     case 1:
+//         //prize = prize + "trip for two and" 
+//         prize += "trip for two and "
+//         break;
+//     case 2:
+//         prize += "four piece furniture"
+//         // prize = "trip for two and four piece of furniture"
+//         break;
+//     case 3: 
+//         prize += " smartwatch and"
+//         break;
+//     default: 
+//         prize += " tickets circus"; 
+//         break;
+// }
+// console.log("you have won " + prize);
+
+// listItems = 1;
+// if(listItems){
+//     console.log('true')
+// } else {
+//     console.log('false');
+// }
+
+// let count = 1;
+// while(count<=10){
+//     console.log("counting " + count );
+        // count++;
+//     //count = count + 1      
+// }
+
+hello();
+
+const hello = function () {
+    console.log("hello");
+}
+
