371425cbcc189843ae329c07cc90940ff47c65d3
Koki Sakai <likuor0528@gmail.com>
2022-05-06 09:44:13 -0700

    Done tile ex25

---
+++ ciccc-js-fundamentals/exercises/ex23.js
@@ -0,0 +1,33 @@
+/*
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+Write a loop under each comment in the file.
+Under the first, write a while loop that prints out each item of ingredients.
+Under the second, write a for loop that does the same thing.
+And under the third, write any loop, while or for that prints out each item of ingredients but backwards (that is, the first should be "bananas" and the last "eggs").
+*/
+
+const ingredients = [
+  'eggs',
+  'milk',
+  'flour',
+  'sugar',
+  'baking soda',
+  'baking powder',
+  'chocolate chips',
+  'bananas',
+];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+console.log('----------');
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+console.log('----------');
+for (let i = 7; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
